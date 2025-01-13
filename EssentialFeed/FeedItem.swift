9e7c1e94ff5df2e9c919c04519b086ce6548dc1a
Mustafa Qutbuddin <mq.kakajiwala@gmail.com>
2025-01-12 16:11:07 -0800

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Mustafa Qutbuddin on 2025-01-12.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
